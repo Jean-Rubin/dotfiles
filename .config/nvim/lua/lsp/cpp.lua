ecc2802e0ce72f787638ef0f029d3c387422c623
Jean-Rubin <rubin.jean98@gmail.com>
2022-12-07 02:11:22 +0100

    Added local mapping for cpp

--- .config/nvim/lua/lsp/cpp.lua
+++ .config/nvim/lua/lsp/cpp.lua
@@ -1,7 +1,19 @@
 local lsp_common_setup = require('lsp.lsp_setup')
 
+local set_local_mappings = function(_, bufnr)
+    vim.keymap.set(
+        { "n" },
+        "<leader>ah",
+        "<Cmd>ClangdSwitchSourceHeader<CR>",
+        { buffer = bufnr }
+    )
+end
+
 require('lspconfig').clangd.setup {
     capabilities = lsp_common_setup.capabilities,
     handlers = lsp_common_setup.handlers,
-    on_attach = lsp_common_setup.on_attach
+    on_attach = function(client, bufnr)
+        lsp_common_setup.on_attach(client, bufnr)
+        set_local_mappings(client, bufnr)
+    end
 }
